ec0ac36148a8a09883f1a01a20bce932a43363ca
Kevin Trogant <greenfoxlight.42@gmail.com>
2016-12-28 17:15:50 +0100

    Made program interactive

--- src/main/app/Stammbaeume.java
+++ src/main/app/Stammbaeume.java
@@ -1,5 +1,9 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * Created by kevintrogant on 15.12.16.
  */
@@ -7,47 +11,25 @@ public class Stammbaeume {
 
     public static void main(String[] args) {
         System.out.println("Stammbäume");
+        System.out.println("Exit with \"exit\" or \"quit\"");
 
 
         UI ui = new UI();
-        /* Case does not matter for commands (Only for names)
-         */
-        ui.parseCommand("INSERT PERSON Bob MALE");
-        ui.parseCommand("INSERT PERSON Eva FEMALE");
-        ui.parseCommand("INSERT PERSON Tom MALE");
-        ui.parseCommand("INSERT PERSON Tim MALE");
-        ui.parseCommand("INSERT PERSON Timon MALE");
-        ui.parseCommand("INSERT PERSON Pumba MALE");
-        ui.parseCommand("INSERT PERSON Tina FEMALE");
-        ui.parseCommand("MAKE Tom CHILD OF Bob");
-        ui.parseCommand("MAKE Tom CHILD OF Eva");
-        ui.parseCommand("MAKE Tim CHILD OF Bob");
-        ui.parseCommand("MAKE Timon CHILD OF Tom");
-        ui.parseCommand("MAKE Pumba CHILD OF Tom");
-        ui.parseCommand("MAKE Tina CHILD OF Tim");
-        ui.parseCommand("MARRY Bob WITH Eva");
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String in = "";
+            boolean run = true;
+            while (run) {
+                System.out.print("> ");
+                in = reader.readLine();
 
-        System.out.println("Structured tree:");
-        ui.parseCommand("PRINT TREE");
-        ui.parseCommand("GET Spouse OF Bob");
-
-        //System.out.println(FamilyTree.getInstance().isParentOf("Bob", "Tom"));
-        ui.parseCommand("IS Bob Parent OF Tom");
-
-        //System.out.println(FamilyTree.getInstance().getChildrenOf("Eva"));
-        ui.parseCommand("GET Children OF Eva");
-
-        // System.out.println(FamilyTree.getInstance().getGrandchildrenOf("Bob"));
-        ui.parseCommand("GET Grandchildren OF Bob");
-
-        //System.out.println(FamilyTree.getInstance().getGrandparentsOf("Timon"));
-        ui.parseCommand("GET Grandparents OF Timon");
-
-        //System.out.println(FamilyTree.getInstance().getCousinsOf("Tina"));
-        ui.parseCommand("GET Cousins OF Tina");
-
-        //System.out.println(FamilyTree.getInstance().getFathersSiblings("Tina"));
-        ui.parseCommand("GET Fathers_Siblings OF Tina");
-
+                run = !in.toUpperCase().equals("EXIT") && !in.toUpperCase().equals("QUIT");
+                if (run) {
+                    ui.parseCommand(in);
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
